2e7902b4cedcbba0c449f5c4e9cbfb4d416115eb
mr poe <127251376+aiden0151@users.noreply.github.com>
2025-08-21 00:40:26 +0100

    h

---
+++ minimal_test.php
@@ -0,0 +1,8 @@
+<?php
+// Ultra-basic PHP test with minimal code
+echo "Hello World - PHP is working!";
+echo "<br>PHP Version: " . phpversion();
+echo "<br>Current date: " . date('Y-m-d H:i:s');
+echo "<br>Server: " . $_SERVER['SERVER_SOFTWARE'];
+echo "<br><strong>If you see this, PHP is functioning correctly.</strong>";
+?>
