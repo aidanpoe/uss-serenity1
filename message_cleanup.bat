c1e8adc605ac37c7c84df6eb6fb6f20c622a56d3
mr poe <127251376+aiden0151@users.noreply.github.com>
2025-09-29 22:27:08 +0100

    k

--- message_cleanup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM USS Serenity Message Cleanup Script for Windows
-REM Schedule this in Windows Task Scheduler to run daily
-
-cd /d "%~dp0"
-php cleanup_messages.php
-
-REM Optional: Log the result
-echo %date% %time%: Message cleanup completed >> logs\cleanup.log
